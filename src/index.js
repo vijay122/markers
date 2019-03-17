27ab43336aae45c0b8c32d936af51c54bd41a81e
Vijay <livelytripsmanagement@gmail.com>
2019-03-17 13:14:09 +0530

    publish

--- src/index.js
+++ src/index.js
@@ -41,9 +41,6 @@ const stepLabel= {
 }
 
 const pinMarker= {
-  //behavior: url(PIE.htc);
-  /* remove if you don't care about IE8  */
-  //paddingBottom:'10px',
   width: '28px',
   height: '28px',
   textAlign: 'center',
